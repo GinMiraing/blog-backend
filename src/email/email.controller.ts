44a015485cfb56989e3b3c703c3900367010ed26
Gin Miraing <ginmiraing@outlook.com>
2023-11-21 10:01:32 +0800

    feat: 新增测试邮件回复的接口

---
+++ src/email/email.controller.ts
@@ -0,0 +1,27 @@
+import { Controller, Get } from '@nestjs/common';
+import { Throttle } from '@nestjs/throttler';
+
+import { EmailService } from './email.service';
+
+@Controller('email')
+export class EmailController {
+  constructor(private readonly emailService: EmailService) {}
+
+  @Throttle({ default: { limit: 5, ttl: 60000 } })
+  @Get()
+  async sendTestEmail() {
+    return this.emailService.sendEmail({
+      parentNick: '胤',
+      parentAvatar:
+        'https://cravatar.cn/avatar/5b787600fd54341c6ddca147754d555b',
+      parentContent:
+        '父评论内容 - 测试内容 - 这是一段比较长的评论内容，仅用于测试。',
+      replyNick: 'GinMiraing',
+      replyAvatar:
+        'https://cravatar.cn/avatar/5b787600fd54341c6ddca147754d555b',
+      replyContent: '这是一段回复内容',
+      url: 'https://blog.zengjunyin.com',
+      address: 'zengjunyin@foxmail.com',
+    });
+  }
+}
